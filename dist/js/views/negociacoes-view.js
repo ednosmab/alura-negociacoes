19dc7cc85fa194893705279efa124f3153033a38
Edson Garcia <ednosmab@yahoo.com.br>
2024-04-03 17:52:05 -0300

    implementing the view class as an abstract to be extended in MessageView and NegotiationsView

--- dist/js/views/negociacoes-view.js
+++ dist/js/views/negociacoes-view.js
@@ -1,7 +1,5 @@
-export class NegociacoesView {
-    constructor(seletor) {
-        this.elemento = document.querySelector(seletor);
-    }
+import { View } from "./view.js";
+export class NegociacoesView extends View {
     template(model) {
         console.log(model.lista());
         return `
